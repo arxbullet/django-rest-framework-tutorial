e7398411eaa60595784158adbc5eaa4c8cc80c7b
arx_bullet <aranzav.c.b@gmail.com>
2022-03-31 23:48:00 -0300

    added views routes

--- profiles_Api_example/urls.py
+++ profiles_Api_example/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from profiles_Api_example import views
+from rest_framework.routers import DefaultRouter 
+
+router = DefaultRouter()
+router.register('hello-viewset', views.helloViewSet, basename='hello-viewset')
 
 urlpatterns = [
     path('hello/', views.helloApiView.as_view()),
+    path('', include(router.urls))
 ]
+
