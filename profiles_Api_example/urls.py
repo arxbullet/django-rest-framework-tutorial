fff0e1ba67256af08188490b24bffc8c4947372a
arx_bullet <aranzav.c.b@gmail.com>
2022-05-06 21:52:33 -0400

    added router for viesets and login apiview path

--- profiles_Api_example/urls.py
+++ profiles_Api_example/urls.py
@@ -3,10 +3,16 @@ from profiles_Api_example import views
 from rest_framework.routers import DefaultRouter 
 
 router = DefaultRouter()
-router.register('hello-viewset', views.helloViewSet, basename='hello-viewset')
 
+#viewsets registro
+router.register('hello-viewset', views.helloViewSet, basename='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
+router.register('feed', views.UserProfileFeedViewSet)
+
+#apiviews registro
 urlpatterns = [
     path('hello/', views.helloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
 
